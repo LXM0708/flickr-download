da7337c1ddb181d34275124272c782caaf1e7dac
Allan Beaufour <allan@beaufour.dk>
2020-03-15 13:15:20 -0400

    removes unnecessary from __future__ imports as the future has arrived :)

--- flickr_download/flick_download.py
+++ flickr_download/flick_download.py
@@ -2,11 +2,6 @@
 #
 # Util to download a full Flickr set.
 #
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
 import argparse
 import errno
 import logging
